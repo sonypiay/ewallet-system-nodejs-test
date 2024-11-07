73aaee7cea0ea94141dba9ef867ce7c665f540bc
sonypiay <pyscho30@gmail.com>
2024-11-08 01:05:12 +0700

    adjust validasi transaction request

--- src/validation/TransactionRequest.js
+++ src/validation/TransactionRequest.js
@@ -4,12 +4,15 @@ import ValidationRequest from "./ValidationRequest.js";
 class TransactionRequest extends ValidationRequest {
     static storeRequest(request) {
         const schema = Joi.object({
-            user_id: Joi.string()
+            user_id: Joi
+                .number()
+                .integer()
                 .required()
                 .messages({
-                    'string.base': 'user_id must be a string',
-                    'string.empty': 'user_id cannot be an empty field',
-                    'string.required': 'Missing parameter user_id'
+                    'number.base': 'user_id must be a number',
+                    'number.integer': 'user_id must be a number',
+                    'number.empty': 'user_id cannot be an empty field',
+                    'number.required': 'Missing parameter user_id'
                 }),
             amount: Joi
                 .number()
