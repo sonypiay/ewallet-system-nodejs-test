edb0192bc00fd09d0bcb38cb395a02700fecd606
sonypiay <pyscho30@gmail.com>
2024-11-08 00:10:46 +0700

    add logic for transaction service

--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -1,19 +1,64 @@
-// import { Connection } from '../application/database-connection.js';
-
+import { ConnectionPrisma } from "../application/Connection.js";
 import ErrorException from "../exceptions/ErrorException.js";
 import NotFoundException from "../exceptions/NotFoundException.js";
+import TransactionRequest from "../validation/TransactionRequest.js";
 
 export const TransactionsService = {};
 
 TransactionsService.credit = async (request) => {
-    throw new ErrorException("Testing error exception");
+    const storeRequest = TransactionRequest.storeRequest(request.body);
 
-    // return {
-    //     body: request.body,
-    //     path: request.path,
-    // };
+    const [resultTransaction] = await ConnectionPrisma.$transaction(async (tx) => {
+        const userId = parseInt(storeRequest.user_id);
+        const amount = parseFloat(storeRequest.amount);
+
+        const currentBalance = await tx.users.findUnique({
+            select: {
+                balance: true
+            },
+            where: {
+                id: userId
+            }
+        });
+
+        if( currentBalance === null ) throw new NotFoundException('No user found');
+
+        const totalAmount = parseFloat(currentBalance.balance) + amount;
+
+        await tx.users.update({
+            data: {
+                balance: totalAmount,
+            },
+            where: {
+                id: userId
+            }
+        });
+
+        const resultTransaction = await tx.transactions.create({
+            data: {
+                user_id: userId,
+                amount: amount,
+                type: 'credit',
+                created_at: new Date(),
+            },
+            select: {
+                id: true,
+                amount: true,
+                created_at: true,
+            }
+        });
+
+        return [resultTransaction];
+    });
+
+    return {
+        status: "success",
+        transaction_id: resultTransaction.id,
+        new_balance: parseFloat(resultTransaction.amount),
+    };
 }
 
 TransactionsService.debit = async (request) => {
-    throw new NotFoundException("Data not found");
+    const storeRequest = TransactionRequest.storeRequest(request.body);
+    return storeRequest;
 }
